37d1b5b4904e4a2c1011f7ec9da7fecba7b3227c
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2020-01-10 18:44:43 -0600

    forgot combo term

--- keyboards/gergo/keymaps/germ/rules.mk
+++ keyboards/gergo/keymaps/germ/rules.mk
@@ -13,6 +13,8 @@ VERBOSE 		 = yes
 DEBUG_MATRIX_SCAN_RATE   = no
 DEBUG_BALLER 		 = no
 DEBUG_MATRIX		 = no
+AUDIO_ENABLE  = yes
+OLED_ENABLE = yes
 
 # A bunch of stuff that you shouldn't touch unless you
 # know what you're doing.
--- keyboards/gergo/keymaps/oled/config.h
+++ keyboards/gergo/keymaps/oled/config.h
@@ -2,3 +2,6 @@
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define OLED_FONT_H "keyboards/gergo/keymaps/oled/glcdfont.c"
+#define OLED_FONT_WIDTH 5
+#define OLED_FONT_HEIGHT 7
+
